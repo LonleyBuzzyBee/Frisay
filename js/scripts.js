2f5f3e03aca2a90dfaa2da3bf8ec1406fe101db2
mai <mailiecook@yahoo.com>
2020-01-17 10:50:33 -0800

    Add div classes coffee,tea,kombucha,water to index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,20 @@
 $(document).ready(function() {
-  $(".quiz").submit(function(event) {
+  $("form").submit(function(event) {
     var beverage = $("#beverage").val();
 
     if (beverage === "1") {
-      $(".1").show();
+      $(".1").alert();
+      $(".2").alert();
+      $(".3").show();
+      $(".4").show();
     }
+    else if (beverage === "2") {
+      $(".1").show();
+      $(".2").show();
+      $(".3").show();
+      $(".4").alert();
+    }
+
   event.preventDefault();
   });
 });
