8828094e579962eac9855f33866d79c7ed601920
mai <mailiecook@yahoo.com>
2020-01-17 10:31:34 -0800

    add button tag to index.html, and corrected syntax in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
-var beverage = $("#beverage").val();
+$(document).ready(function() {
+  $(".quiz").submit(function(event) {
+    var beverage = $("#beverage").val();
+
+    if (beverage === "1") {
+      $(".1").show();
+    }
+  event.preventDefault();
+  });
+});
