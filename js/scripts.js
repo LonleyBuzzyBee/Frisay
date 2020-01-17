41928ef6a106741bb553ed94d8027d33b722acd9
mai <mailiecook@yahoo.com>
2020-01-17 12:29:43 -0800

    correct comparison operators in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
   $("form#quiz").submit(function(event) {
-    var beverage = parseInt($("#beverage").val()); 
-    var score = beverage
+    var b1 = parseInt($("#b1").val()); 
+    var b2 = parseInt($("#b2").val()); 
+    var score = add(b1,b2)
 
-    if (beverage === "1") {
-      $("1").alert();
-      $("2").alert();
-      $("3").show();
-      $("4").show();
+    if (score <= 2) {
+      $(".coffee").alert();
+      $(".tea").hide();
     }
-    else if (beverage === "2") {
-      $("1").show();
-      $("2").show();
-      $("3").show();
-      $("4").alert();
+    else if (score > 2 && score < 4) {
+      $(".tea").alert();
+      $(".coffee").hide();
     }
-
+    else if (score > 3) {
+      $(".tea").show();
+      $(".coffee").hide();
+    }
   event.preventDefault();
   });
 });
