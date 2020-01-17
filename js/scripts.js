98bc6ec9e9b4bc771c852f54c0cbb11d45ff2183
mai <mailiecook@yahoo.com>
2020-01-17 13:08:11 -0800

    fix errors on line 5 in scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,14 @@ $(document).ready(function() {
   $("form#quiz").submit(function(event) {
     var b1 = parseInt($("#b1").val()); 
     var b2 = parseInt($("#b2").val()); 
-    var score = add(b1,b2)
-
+    var score = b1+b2;
+    
     if (score <= 2) {
-      $(".coffee").alert();
+      $(".coffee").show();
       $(".tea").hide();
     }
-    else if (score > 2 && score < 4) {
-      $(".tea").alert();
+    else if (score === 3) {
+      $(".tea").show();
       $(".coffee").hide();
     }
     else if (score > 3) {
