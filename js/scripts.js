c86458bbfe80ad80c40776c417144c2ff19b7528
mai <mailiecook@yahoo.com>
2020-01-17 13:18:23 -0800

    add lines 24 to 43, ids q3,q4,q5,q6, in index.html. add vars q3,q4,q5,q6 to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
   $("form#quiz").submit(function(event) {
-    var b1 = parseInt($("#b1").val()); 
-    var b2 = parseInt($("#b2").val()); 
-    var score = b1+b2;
+    var q1 = parseInt($("#q1").val()); 
+    var q2 = parseInt($("#q2").val()); 
+    var q3 = parseInt($("#q3").val()); 
+    var q4 = parseInt($("#q4").val()); 
+    var q5 = parseInt($("#q5").val()); 
+    var q6 = parseInt($("#q6").val()); 
+    var score = q1+q2+q3+q4+q5+q6;
     
     if (score <= 2) {
       $(".coffee").show();
