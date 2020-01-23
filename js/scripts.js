95e19c450643a570d7862b7f09f39777803891f0
mai <mailiecook@yahoo.com>
2020-01-22 19:47:11 -0800

    Add links in index.html, add info box in styles.css, add .click method in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
   $("form#quiz").submit(function(event) {
-    var q1 = parseInt($("#q1").val()); 
-    var q2 = parseInt($("#q2").val()); 
-    var q3 = parseInt($("#q3").val()); 
-    var q4 = parseInt($("#q4").val()); 
-    var q5 = parseInt($("#q5").val()); 
-    var q6 = parseInt($("#q6").val()); 
+    event.preventDefault();
+    var q1 = parseInt($("#q1").val());
+    var q2 = parseInt($("#q2").val());
+    var q3 = parseInt($("#q3").val());
+    var q4 = parseInt($("#q4").val());
+    var q5 = parseInt($("#q5").val());
+    var q6 = parseInt($("#q6").val());
     var score = q1+q2+q3+q4+q5+q6;
 
     if (score < 7) {
@@ -32,7 +33,18 @@ $(document).ready(function() {
       $(".ruby").hide();
       $(".c").hide();
     }
-    
-  event.preventDefault();
+  });
+
+  $(".btn-lang").click(function() {
+    var lang = $(this).text().toLowerCase().replace("#", "");
+    $(".info-box-wrap").css('display', 'flex');
+    $('.info-box').show();
+    $("#info-" + lang).show();
+  });
+
+  $(".info-box-close").click(function() {
+    $(".info-box-wrap").css('display', 'none');
+    $('.info-box').hide();
+    $(".info-text").hide();
   });
 });
